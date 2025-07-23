4744fd2ca796fe95911b092c665e9dcb5493ef3d
Jonathan <jda_phys@hotmail.co.uk>
2025-07-24 00:05:07 +0100

    updated vite config public path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/myapps/',
   plugins: [react()],
   build: {
     outDir: 'docs',      // ← this makes `npm run build` emit into ./docs
