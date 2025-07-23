8ce5c3c868f1ed1acfe00598e302fbb2604a446c
Jonathan <jda_phys@hotmail.co.uk>
2025-07-23 23:57:34 +0100

    Added outdir 'docs' to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',      // ← this makes `npm run build` emit into ./docs
+  },
   server: {
     proxy: {
       '/api': {
